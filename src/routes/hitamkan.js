9958582c2bd666b6e10a48680f8d42c0d1f639b5
Arnov77 <arilnove75@gmail.com>
2025-04-07 10:41:32 +0700

    Update hitamkan.js

--- src/routes/hitamkan.js
+++ src/routes/hitamkan.js
@@ -1,7 +1,23 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const { blackWaifuGemini } = require("../controllers/blackWaifuGemini");
+const { blackWaifuGemini } = require('../controllers/blackWaifuGemini');
+const config = require('../../config');
 
-router.get("/", blackWaifuGemini);
+// Endpoint untuk menghitamkan waifu
+router.all('/', async (req, res) => {
+  if (!['GET', 'POST'].includes(req.method)) {
+    return res.status(405).json({ status: 405, message: 'Method Not Allowed' });
+  }
+
+  try {
+    const obj = req.method === 'GET' ? req.query : req.body;
+    req.query.image = obj.image; // Set ulang query biar kompatibel dengan controller
+    const response = await blackWaifuGemini(req, res);
+    return response; // biar sesuai dengan controller-nya yang handle response langsung
+  } catch (e) {
+    console.error(e);
+    res.status(500).json({ status: 500, message: utils.getError(e) });
+  }
+});
 
 module.exports = router;

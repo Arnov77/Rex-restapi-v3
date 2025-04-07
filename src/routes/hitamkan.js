5a2e53559f0fe490ef1417c99412d39240b5d541
Arnov77 <arilnove75@gmail.com>
2025-04-07 10:08:37 +0700

    Update hitamkan.js

--- src/routes/hitamkan.js
+++ src/routes/hitamkan.js
@@ -1,57 +1,24 @@
 const express = require('express');
 const router = express.Router();
-const multer = require('multer');
-const axios = require('axios');
 const utils = require('../utils/utils');
 const config = require('../../config');
 
-const upload = multer();
-
-// POST: kirim file langsung
-router.post('/', upload.single('image'), async (req, res) => {
-  try {
-    if (!req.file) {
-      return res.status(400).json({ status: 400, message: "Parameter 'image' (file upload) diperlukan" });
-    }
-
-    const resultUrl = await utils.generateHitamkanWaifu(req.file.buffer);
-
-    res.json({
-      status: 200,
-      creator: config.creator,
-      data: {
-        imageUrl: resultUrl,
-      },
-    });
-  } catch (e) {
-    console.error(e);
-    res.status(500).json({ status: 500, message: utils.getError(e) });
-  }
-});
-
-// GET: ambil gambar dari URL
 router.get('/', async (req, res) => {
+  const { image } = req.query;
+  if (!image) {
+    return res.status(400).json({ status: 400, message: "Parameter 'image' diperlukan" });
+  }
+
   try {
-    const imageUrl = req.query.image;
-    if (!imageUrl) {
-      return res.status(400).json({ status: 400, message: "Parameter 'image' (URL) diperlukan" });
-    }
-
-    const response = await axios.get(imageUrl, { responseType: 'arraybuffer' });
-    const imageBuffer = Buffer.from(response.data, 'binary');
-
-    const resultUrl = await utils.generateHitamkanWaifu(imageBuffer);
-
+    const url = await utils.blackenWaifuFromURL(image);
     res.json({
       status: 200,
       creator: config.creator,
-      data: {
-        imageUrl: resultUrl,
-      },
+      data: { imageUrl: url },
     });
-  } catch (e) {
-    console.error(e);
-    res.status(500).json({ status: 500, message: utils.getError(e) });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ status: 500, message: utils.getError(err) });
   }
 });
 

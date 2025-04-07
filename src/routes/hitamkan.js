c3d9903226ec0a5d64713d8425db92ba7ea85fd6
Arnov77 <arilnove75@gmail.com>
2025-04-07 10:36:31 +0700

    Update hitamkan.js

--- src/routes/hitamkan.js
+++ src/routes/hitamkan.js
@@ -1,25 +1,7 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const utils = require('../utils/utils');
-const config = require('../../config');
+const { blackWaifuGemini } = require("../controllers/blackWaifuGemini");
 
-router.all('/', async (req, res) => {
-  const { image } = req.query;
-  if (!image) {
-    return res.status(400).json({ status: 400, message: "Parameter 'image' diperlukan" });
-  }
-
-  try {
-    const url = await utils.blackenWaifuFromURL(image);
-    res.json({
-      status: 200,
-      creator: config.creator,
-      data: { imageUrl: url },
-    });
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({ status: 500, message: utils.getError(err) });
-  }
-});
+router.get("/", blackWaifuGemini);
 
 module.exports = router;

27d75468c742b6a4c8fa7413701babef7e0ca232
Arnov77 <arilnove75@gmail.com>
2025-04-16 19:56:26 +0700

    Update utils.js

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -59,11 +59,8 @@ const utils = {
       await page.click('#toggleButtonWhite');
       await page.locator('#textInput').fill(text);
       await page.evaluate(() => {
-      const el = document.getElementById('textOverlay');
-      el.style.width = '512px';
-      el.style.height = '512px';
-  });
-      const screenshotBuffer = await page.locator('#textOverlay').screenshot();
+     
+      const screenshotBuffer = await page.locator('#memeImage').screenshot();
       return await utils.uploadToTmpfiles(screenshotBuffer, `${utils.randomName('.jpg')}`);
     } finally {
       if (browser) await browser.close();

b704a0ee755ff02f567b561e9a80686bea062c5c
Arnov77 <arilnove75@gmail.com>
2025-04-18 16:35:23 +0700

    Add consent button handling in utils for improved user experience

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -49,6 +49,13 @@ const utils = {
     try {
       const page = await browser.newPage();
       await page.goto("https://www.bratgenerator.com/");
+      
+      const acceptButton = page.locator('#onetrust-accept-btn-handler');
+      if ((await acceptButton.count()) > 0 && await acceptButton.isVisible()) {
+        await acceptButton.click();
+        await page.waitForTimeout(500);
+      }
+
       await page.click('#toggleButtonWhite');
       await page.locator('#textInput').fill(text);
       const screenshotBuffer = await page.locator('#textOverlay').screenshot();

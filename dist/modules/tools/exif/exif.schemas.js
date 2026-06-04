118c9a5b7fbc1fd145860021ff73af7f5779e4c1
Arnov77 <arilnove75@gmail.com>
2026-06-04 17:18:59 +0800

    untrack dist

--- dist/modules/tools/exif/exif.schemas.js
+++
@@ -1,9 +0,0 @@
-import { z } from 'zod';
-/**
- * EXIF
- * Extract metadata from an image URL.
- */
-export const ExifQuery = z.object({
-    image: z.string().url().max(2048),
-});
-//# sourceMappingURL=exif.schemas.js.map

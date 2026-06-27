27912d22031b170b66c829e3d08c90997e58aff0
Arnov77 <arilnove75@gmail.com>
2026-06-27 18:23:15 +0800

    add nsfw dtector ednpoint

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import { buildApp } from './app.js';
 import { loadEnv } from './config/env.js';
 import { ensureMasterKeyBootstrap } from './bootstrap.js';
+import { loadNsfwModel } from './modules/tools/nsfw/nsfw.service.js';
 
 async function main() {
   const env = loadEnv();
@@ -15,6 +16,10 @@ async function main() {
     app.log.error({ err }, 'master key bootstrap failed');
     process.exit(1);
   }
+
+  loadNsfwModel().catch((err) => {
+    app.log.warn({ err }, '[nsfw] model gagal preload, akan retry saat request pertama');
+  });
 
   const shutdown = async (signal: string) => {
     app.log.info({ signal }, 'shutting down');
